76457607f6a65a54f291a9d577fb4a7e6cc0a2af
Pratik Thorat <pratik.thorat@techno-soft.in>
2021-05-14 11:33:47 +0530

    created repo

---
+++ Components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TodoList = () => {
+    return(
+        <View>
+            <Text>Todo List</Text>
+        </View>
+    )
+}
+
+
+export default TodoList;
